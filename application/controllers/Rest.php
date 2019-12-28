0345e424a58fa857f53bd5bd973a057ea1b427b2
happybit <wangleizi>
2019-12-28 20:14:24 +0800

    fix flow 3

--- application/controllers/Rest.php
+++ application/controllers/Rest.php
@@ -23,21 +23,23 @@ class Rest extends QB_Controller{
 	}
 
   public function getUploadId(){
-    $user_key = 'whoami';
+    $user_key = isset($_POST['user_id']) ? $_POST['user_id'] : 'whoami';
     $upload_id = md5( $user_key . time() );
     //todo 记录一条upload_id 到数据库
     $this->restResult(['upload_id'=>$upload_id]);
 	}
 	public function upload2OSS(){
-    //0、todo 先判断是否有upload_id
-    $upload_id = $_POST['upload_id'] ;
+    
     //1、处理上传的分片文件
-    if(!isset($_FILES['blob']) || !isset($_FILES['upload_id'])){
+    if(!isset($_FILES['blob']) || !isset($_POST['upload_id'])){
       $this->restResult('Less Param blob or upload_id',[],500);
     }
+    //1.2、todo 先判断是否有upload_id
+    $upload_id = $_POST['upload_id'] ;
+
     //上传buffer中
     $file = $_FILES['blob'];
-    $file_md5 = file_md5($file);
+    $file_md5 = md5_file($file['tmp_name']);
     $file_index = $_POST['file_index'] ;
     $this->restResult(['file_md5'=>$file_md5]);
 	}
@@ -46,6 +48,7 @@ class Rest extends QB_Controller{
     //判断upload_id 是否存在
     $upload_id = $_POST['upload_id'] ;
     $file_count = isset($_POST['file_count']) ? intval($_POST['file_count']) : null ;
+    $this->restResult('verifyUpload');
   }
   
 }
